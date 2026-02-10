ed9150ece76ca3d17e2dcb2875fd58e6f63987a5
RapierCraft <rapiercraftstudios@gmail.com>
2026-02-10 08:34:37 +0000

    chore: add .gitignore, remove node_modules and dist from tracking

--- node_modules/n8n-nodes-alterlab/dist/credentials/AlterLabOAuth2Api.credentials.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ICredentialType, INodeProperties } from 'n8n-workflow';
-export declare class AlterLabOAuth2Api implements ICredentialType {
-    name: string;
-    displayName: string;
-    documentationUrl: string;
-    extends: string[];
-    properties: INodeProperties[];
-}
-//# sourceMappingURL=AlterLabOAuth2Api.credentials.d.ts.map
